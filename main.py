616ccd8e79e22ad5b771ee4cdfd3d0ea240742c0
Sanket4901 <86614636+Sanket4901@users.noreply.github.com>
2022-01-08 20:38:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -275,37 +275,6 @@ def sell():
 
 
 ###################################################################################
-# import smtplib
-
-# my_mail = "stockeye12@gmail.com"
-# username = "rohitislife45@gmail.com"
-# password = "Stockeye@1122"
-# connection = smtplib.SMTP("smtp.gmail.com")
-# connection.starttls()
-# connection.login(user=my_mail, password=password)
-
-
-# api = "https://newsapi.org/v2/everything"
-# key = "3e0aecb9400946298af7aedaeee87497"
-
-# company = "sbin"
-# para = {
-#     "qInTitle": company,
-#     "apiKey": key,
-# }
-# r = requests.get(url=api, params=para)
-# articles = r.json()["articles"]
-# three_articles = articles[:3]
-# list = [
-#     f"Headlines :{article['title']}.\n Brief:{article['description']}"
-#     for article in three_articles
-# ]
-# for x in list:
-#     connection.sendmail(from_addr=my_mail, to_addrs=username, msg=x)
-#     connection.close()
-
-
-###################################################################################
 
 if __name__ == "__main__":
     app.run(debug=True)
